29950c7adccfffed8833afbd7a168d93ea4f9c74
Amanda Gilmore <amg@Amandas-MacBook-42.local>
2017-01-11 13:29:48 -0800

    fixed spacing

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,31 +1,31 @@
 class Squares
 # RED GREEN REFACTOR
-	def initialize(num)
-		@range = num
-	end
+  def initialize(num)
+    @range = num
+  end
 
-	def square_of_sum
-		iterator = @range
-		total = 0
-		while iterator > 0
-			total += iterator
-			iterator -= 1
-		end
-		total**2		
-	end
+  def square_of_sum
+    iterator = @range
+    total = 0
+    while iterator > 0
+      total += iterator
+      iterator -= 1
+    end
+    total**2    
+  end
 
-	def sum_of_squares
-		iterator = @range
-		total = 0
-		while iterator > 0
-			total += iterator**2
-			iterator -= 1
-		end
+  def sum_of_squares
+    iterator = @range
+    total = 0
+    while iterator > 0
+      total += iterator**2
+      iterator -= 1
+    end
 
-		total
-	end
+    total
+  end
 
-	def difference
-		square_of_sum - sum_of_squares
-	end
+  def difference
+    square_of_sum - sum_of_squares
+  end
 end

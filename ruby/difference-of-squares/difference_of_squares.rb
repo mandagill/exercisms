b94f4df76c4fde1d46385d822b87ef5557fed8d4
Amanda Gilmore <amg@Amandas-MacBook-42.local>
2017-01-10 16:53:07 -0800

    Stubbed out Squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,15 @@
+class Squares
+
+	def initialize(num)
+		@range = num
+		@sum = sum_of_squares(num)
+	end
+
+	def square_of_sum
+		# This will be an object method
+	end
+
+	def sum_of_squares(num)
+		# recurse until num is 0
+	end
+end

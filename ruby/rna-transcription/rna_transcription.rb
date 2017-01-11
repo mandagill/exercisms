cc6e6c7f6900166ff7ebadccfe648c4f15a850b7
Amanda Gilmore <amg@Amandas-MacBook-42.local>
2017-01-11 13:52:57 -0800

    refactor in progress

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,26 +1,25 @@
 class Complement
 
-  NUCLEOTIDES = ["G", "C", "T", "A"]
-
   def self.of_dna(dna_string)
 
     dna_string.upcase!
-    dna_arr = dna_string.split("")
     rna_string = ""
 
-    dna_arr.each do |nucleotide|
-      if !Complement::NUCLEOTIDES.include?(nucleotide)
-        return ""
-      elsif nucleotide == "G"
+    dna_string.each_char {|nucleotide|
+      
+      case nucleotide        
+      when nucleotide == "G"
         rna_string << "C"
-      elsif nucleotide == "C"
+      when nucleotide == "C"
         rna_string << "G"
-      elsif nucleotide == "T"
+      when nucleotide == "T"
         rna_string << "A"
-      elsif nucleotide == "A"
+      when nucleotide == "A"
         rna_string << "U"
+      else
+        return ""
       end
-    end
+    }
 
     rna_string
 

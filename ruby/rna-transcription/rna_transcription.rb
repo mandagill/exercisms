a8948c946b373cb6915ddba917cd8d6232cce5af
Amanda Gilmore <amg@Amandas-MacBook-42.local>
2017-01-11 13:32:18 -0800

    fixed spacing

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,28 +1,28 @@
 class Complement
 
-	NUCLEOTIDES = ["G", "C", "T", "A"]
+  NUCLEOTIDES = ["G", "C", "T", "A"]
 
-	def self.of_dna(dna_string)
+  def self.of_dna(dna_string)
 
-		dna_string.upcase!
-		dna_arr = dna_string.split("")
-		rna_string = ""
+    dna_string.upcase!
+    dna_arr = dna_string.split("")
+    rna_string = ""
 
-		dna_arr.each do |nucleotide|
-			if !Complement::NUCLEOTIDES.include?(nucleotide)
-				return ""
-			elsif nucleotide == "G"
-				rna_string << "C"
-			elsif nucleotide == "C"
-				rna_string << "G"
-			elsif nucleotide == "T"
-				rna_string << "A"
-			elsif nucleotide == "A"
-				rna_string << "U"
-			end
-		end
+    dna_arr.each do |nucleotide|
+      if !Complement::NUCLEOTIDES.include?(nucleotide)
+        return ""
+      elsif nucleotide == "G"
+        rna_string << "C"
+      elsif nucleotide == "C"
+        rna_string << "G"
+      elsif nucleotide == "T"
+        rna_string << "A"
+      elsif nucleotide == "A"
+        rna_string << "U"
+      end
+    end
 
-		rna_string
+    rna_string
 
-	end
+  end
 end

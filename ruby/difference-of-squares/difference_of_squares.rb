5c9e8cbeccde7753bbac93418e57bf841fab8410
Amanda Gilmore <amg@Amandas-MacBook-42.local>
2017-01-11 13:12:17 -0800

    Squares tests all passing

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,15 +1,31 @@
 class Squares
-
+# RED GREEN REFACTOR
 	def initialize(num)
 		@range = num
-		@sum = sum_of_squares(num)
 	end
 
 	def square_of_sum
-		# This will be an object method
+		iterator = @range
+		total = 0
+		while iterator > 0
+			total += iterator
+			iterator -= 1
+		end
+		total**2		
 	end
 
-	def sum_of_squares(num)
-		# recurse until num is 0
+	def sum_of_squares
+		iterator = @range
+		total = 0
+		while iterator > 0
+			total += iterator**2
+			iterator -= 1
+		end
+
+		total
+	end
+
+	def difference
+		square_of_sum - sum_of_squares
 	end
 end

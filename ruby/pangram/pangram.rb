d93a91a14450d0d13c846e4eb1b60340831e837c
Amanda Gilmore <ms.amgilmore@gmail.com>
2017-01-25 09:35:20 -0800

    pangram WIP, some debug statements

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,6 +1,13 @@
 class Pangram
 
-	def self.pangram?
-
+	def self.pangram?(phrase)
+    puts phrase.match(/[a-z\ ]+/).inspect
+    case phrase
+    when /a-z+\s/
+      true
+    else
+      puts "about to return false"
+      false
+    end
 	end
 end

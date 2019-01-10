66d883b4d390fdbfdcab7e2074d4ea186b22a2c0
Amanda Gilmore <ms.amgilmore@gmail.com>
2019-01-09 20:05:20 -0800

    Added check for products > 9

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -13,8 +13,23 @@ class Luhn(object):
             ccn = ccn.replace(" ", "")
             ccn = deque(ccn[:-1])
             ccn_list_of_ints = [int(d) for d in ccn]
+            products = []
 
-            products = [int(n)*2 for n in ccn_list_of_ints[::-2]]
+            for n in ccn_list_of_ints[::-2]:
+                squared = int(n)*2
+                if squared > 9:
+                    char = str(squared)
+                    digits = [int(char) for char in str(n*2)]
+                    print("this is digits and the type of its contents:",
+                          digits, type(digits[0]))
+                    products.append(sum(digits))
+                    continue
+                else:
+                    products.append(squared)
+
+            print("this is list of products", products)
             return sum(products) % 10 == 0
+
         except:
-            raise Exception("this isn't doing what we want")
+            raise Exception(
+                "Please submit a string of numbers and spaces only")

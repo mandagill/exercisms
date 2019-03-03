a8e963dad8b953d800c62abb925dfeb954ee620a
Amanda Gilmore <ms.amgilmore@gmail.com>
2019-03-03 14:37:15 -0800

    implemented Scrabble™ score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,33 @@
+def build_dict():
+    """this builds a dictionary using int as a key, and a set of
+    str as value. The int represents the point value of the
+    letters in its corresponding set."""
+
+    # It bothers me that this is repetitive but I'm not
+    # sure what a DRYer/prettier way might be since Scrabble
+    # point value categories aren't linear.
+    scoring_index = {}
+    scoring_index[1] = set('AEIOULNRST')
+    scoring_index[2] = set('DG')
+    scoring_index[3] = set('BCMP')
+    scoring_index[4] = set('FHVWY')
+    scoring_index[5] = set('K')
+    scoring_index[8] = set('JX')
+    scoring_index[10] = set('QZ')
+    return scoring_index
+
+
+def score_lookup(letter):
+    table = build_dict()
+    for i in table:
+        if table[i].__contains__(letter):
+            return i
+
+
 def score(word):
-    pass
+    s = word.upper()
+    iterable_word = list(s)
+    score = 0
+    for l in iterable_word:
+        score += score_lookup(l)
+    return score
